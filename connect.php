0033847d06a0cb150a3e3bd943aaf85a18c63730
Br0n1k <Bron1ck337@gmail.com>
2021-06-29 01:41:25 +0300

    add some backend and goods in db

--- connect.php
+++ connect.php
@@ -1,1 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "handmade_stuff"
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM goods";
+$result = mysqli_query($conn, $sql);
