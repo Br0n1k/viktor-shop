99dc221a1741a3ca137d3d5f9e0270869c1877da
Br0n1k <Bron1ck337@gmail.com>
2021-08-11 17:08:37 +0300

    goods edit markup

--- admin/goods.php
+++ admin/goods.php
@@ -58,25 +58,55 @@
             <?php while($row_cat_out = mysqli_fetch_assoc($result_cat_out)): 
                //goods here
             ?>
-               <div class="card-wrap">
-                  <div class="item-card">
-                     <div class="card-line">
-                        <div class="item-name">
-                              <a href="item.php?item=<?php echo $row_cat_out["id"]; ?>"><b><?php echo $row_cat_out["name"]; ?></b></a>
-                        </div>
-                        <div class="item-time"><?php echo $row_cat_out["ordertime"]; ?></div>
-                     </div>
-                     <div class="item-img">
-                        <a href="item.php?item=<?php echo $row_cat_out["id"]; ?>"><img src="<?php echo $row_cat_out["img"]; ?>" alt=""></a>
-                     </div>
-                     <div class="item-desc"><?php echo $row_cat_out["description"]; ?></div>
-                     <div class="card-line card-line-bottom">
-                        <div class="item-price"><?php echo $row_cat_out["cost"]; ?> грн.</div>
-                        <div class="plus-one"></div>
-                        <div class="item-button"><a href="javascript:void(0);" data-id="<?php echo $row_cat_out["id"]; ?>" onclick=addToCart(this);>В корзину</a></div>
-                     </div>
+            <hr>
+               <form action="/admin/create.php" method="post" enctype="multipart/form-data" class="goods-form">
+                  <div class="item-name">
+                     Ссылка на сайте: <a href="/item.php?item=<?php echo $row_cat_out["id"]; ?>"><b><?php echo $row_cat_out["name"]; ?></b></a>
+                  </div><br>
+                  <label>Название:<br>
+                     <input type="text" name="ru_name" value="<?php echo $row_cat_out["name"]; ?>">
+                  </label><br><br>
+                  <label for="ordertime">Время заказа:</label><br>
+                  <select name="ordertime" id="ordertime">
+                     <option value="order">Пiд замовлення</option>
+                     <option value="stock" 
+                     <?php 
+                        $f = $row_cat_out["ordertime"];
+                        if($f == "У наявностi"){
+                           echo "selected";
+                        }
+                  ?>>У наявностi</option>
+                  </select><br><br>
+                  <div class="item-img-big">Основное: <br>
+                     <img src="<?php echo $row_cat_out["img"]; ?>" alt="">
                   </div>
-               </div>
+                  <div class="item-imgs">Дополнительные: <br>
+                     <img src="<?php echo $row_cat_out["img1"]; ?>" alt="">
+                     <img src="<?php echo $row_cat_out["img2"]; ?>" alt="">
+                     <img src="<?php echo $row_cat_out["img3"]; ?>" alt="">
+                     <img src="<?php echo $row_cat_out["img4"]; ?>" alt="">
+                  </div><br>
+                  <label for="bigimg">*Фото (основное, желательно 3х2):</label><br>
+                  <input type="file" name="bigimg" id="bigimg"><br><br>
+                  <label for="img1">Фото дополнительное 1:</label><br>
+                  <input type="file" name="img1" id="img1"><br><br>
+                  <label for="img2">Фото дополнительное 2:</label><br>
+                  <input type="file" name="img2" id="img2"><br><br>
+                  <label for="img3">Фото дополнительное 3:</label><br>
+                  <input type="file" name="img3" id="img3"><br><br>
+                  <label for="img4">Фото дополнительное 4:</label><br>
+                  <input type="file" name="img4" id="img4"><br><br>
+                  <div class="item-desc">
+                     <label>*Описание товара:</label><br>
+                     <textarea name="description" rows="4" cols="50" required ><?php echo $row_cat_out["description"]; ?></textarea><br><br>
+                  </div>
+                  <div class="item-price">
+                     <label for="cost">*Цена ("грн." уже дописано):</label><br>
+                     <input type="text" name="cost" id="cost" required value="<?php echo $row_cat_out["cost"]; ?>">
+                  </div><br>
+                  <button name="good_upd" type="submit">Изменить</button>
+                  <button name="good_del" type="submit">Удалить</button>
+               </form>
 
             <?php endwhile; ?>
 
--- admin/update.php
+++ admin/update.php
@@ -46,20 +46,25 @@
 
         header('Location: /admin/categories.php');
     }
-    // elseif (isset($_POST["cat_del"])){
-    //     $name = $_POST["name"];
-    //     $ru_name = $_POST["ru_name"];
-    //     $desc = $_POST["desc"];
+    elseif (isset($_POST["cat_del"])){
+        $id = $_POST["id"];
 
-    //     $sql = "UPDATE `categories` SET `ru_name` = '$ru_name', `description` = '$desc' WHERE `name` = '$name'";
-    //     mysqli_query($conn, $sql);
-    //     header('Location: /admin/categories.php');
+        if($id != 1){
+            $sql_oldname = mysqli_query($conn, "SELECT `name` FROM categories WHERE `id` = '$id'");
+            while($oldname_result = mysqli_fetch_assoc($sql_oldname)){
+                $oldname = $oldname_result["name"];
+            }
+            $sql_catupdate = "UPDATE `goods` SET `category` = '99else' WHERE `category` = '$oldname'";
+            mysqli_query($conn, $sql_catupdate);
+
+            $sql = "DELETE FROM `categories` WHERE `id` = '$id'";
+            mysqli_query($conn, $sql);
+        }
+
+        header('Location: /admin/categories.php');
+    }
 
 
-    // }
-
-
-//IF CATEGORY NULL ???
 
 
 
